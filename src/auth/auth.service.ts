d58d58d761199e9bff30182a31d566291f3a70cf
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2024-12-16 20:24:16 -0300

    Alterações para a continuação do módulo de autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,39 +1,92 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { JwtService, JwtVerifyOptions } from '@nestjs/jwt';
+import { User } from 'src/repository/entities/User.entity';
+import { PostgreSQLTokens } from 'src/repository/postgresql.enums';
+import { Crypt } from 'src/utils/crypt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     private readonly jwtService: JwtService,
+    @Inject(PostgreSQLTokens.USER_REPOSITORY)
+    private userRepository: Repository<User>,
   ) { }
 
-  async createToken(user: any) {
-    return this.jwtService.sign(user);
+  async createToken(user: User) {
+    return this.jwtService.sign({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },{
+      subject: user.id,
+      issuer: 'task-hive',
+      audience: 'task-hive', // Nível do usuário
+      expiresIn: '90d',// 3 meses
+    });
   }
 
-  async checkToken(token: string) {
-    return this.jwtService.verify(token);
+  async checkToken(token: string, options?: JwtVerifyOptions) {
+    try {
+      
+      return this.jwtService.verify(token, options);
+
+    } catch (error) {
+      throw new BadRequestException('Token inválido')
+    }
   }
 
-  async login(){
+  /**
+   * 
+   * @param email string
+   * @param password string
+   * 
+   * @returns 
+   */
+  async login(email: string, password: string) {
+
+    const user = await this.findFirstUserByEmail(email)
+
+    if(!user) {
+      throw new BadRequestException('Usuário não cadastrado')
+    }
+
+    if(user.password !== await Crypt.hash(password)) {
+      throw new BadRequestException('Senha incorreta')
+    }
+
+
 
   }
 
-  async logout(){
+  async logout() {
 
   }
 
-  async forgetPassword(){
+  async forgetPassword(email: string) {
+    const user = await this.findFirstUserByEmail(email)
+
+    if(!user) {
+      throw new BadRequestException('Usuário não cadastrado')
+    }
+  }
+
+  async resetPassword() {
 
   }
 
-  async resetPassword(){
+  async checkTokenResetPassword() {
 
   }
 
-  async checkTokenResetPassword(){
-
+  async findFirstUserByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: {
+        email,
+        deletedAt: null,
+      },
+    });
   }
 
 }
